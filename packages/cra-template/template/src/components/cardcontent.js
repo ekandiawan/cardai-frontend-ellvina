16be44fbfc88da9bbc36fa27e58149960d45658e
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-06 01:34:34 +0800

    reverse 2

--- packages/cra-template/template/src/components/cardcontent.js
+++ packages/cra-template/template/src/components/cardcontent.js
@@ -1,72 +1,72 @@
-// import React from "react";
-
-// import cardContentData from "../utils/card-content-data";
-// import { Col, Row } from "react-bootstrap";
-
-// const CardContent = () => (
-//   <div className="next-steps my-5">
-//     <h2 className="my-5 text-center">This is CardAI Card page</h2>
-//     <Row className="d-flex justify-content-between">
-//       {cardContentData.map((col, i) => (
-//         <Col key={i} md={5} className="mb-4">
-//           <h6 className="mb-3">
-//             <a target="_blank" rel="noopener noreferrer" href={col.cardImage}>
-//               {col.cardName}
-//             </a>
-//           </h6>
-//           <img src={col.cardImage} class="card__image" />
-//           <p>{col.cardDescription}</p>
-//         </Col>
-//       ))}
-//     </Row>
-//   </div>
-// );
-
-// export default CardContent;
-
 import React from "react";
-import $ from "jquery";
 
-export default class cardContent extends React.Component {
-  constructor(props) {
-    super(props);
+import cardContentData from "../utils/card-content-data";
+import { Col, Row } from "react-bootstrap";
 
-    this.state = {
-      cardName: "",
-      cardImage: "",
-      cardDescription: "",
-    };
-  }
+const CardContent = () => (
+  <div className="next-steps my-5">
+    <h2 className="my-5 text-center">This is CardAI Card page</h2>
+    <Row className="d-flex justify-content-between">
+      {cardContentData.map((col, i) => (
+        <Col key={i} md={5} className="mb-4">
+          <h6 className="mb-3">
+            <a target="_blank" rel="noopener noreferrer" href={col.cardImage}>
+              {col.cardName}
+            </a>
+          </h6>
+          <img src={col.cardImage} class="card__image" />
+          <p>{col.cardDescription}</p>
+        </Col>
+      ))}
+    </Row>
+  </div>
+);
 
-  componentDidMount() {
-    this.fetch();
-  }
+export default CardContent;
 
-  fetch() {
-    var context = this;
+// import React from "react";
+// import $ from "jquery";
 
-    $.ajax({
-      url: "https://calm-lowlands-56636.herokuapp.com/card",
-      method: "GET",
-      success: function (response) {
-        context.setState({
-          cardName: response.cardName,
-          cardImage: response.cardImage,
-          cardDescription: response.cardDescription,
-        });
-      },
-    });
-  }
+// export default class cardContent extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-  render() {
-    return (
-      <div>
-        <h1>
-          {this.state.cardName}
-          {this.state.cardImage}
-          {this.state.cardDescription}
-        </h1>
-      </div>
-    );
-  }
-}
+//     this.state = {
+//       cardName: "",
+//       cardImage: "",
+//       cardDescription: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.fetch();
+//   }
+
+//   fetch() {
+//     var context = this;
+
+//     $.ajax({
+//       url: "https://calm-lowlands-56636.herokuapp.com/card",
+//       method: "GET",
+//       success: function (response) {
+//         context.setState({
+//           cardName: response.cardName,
+//           cardImage: response.cardImage,
+//           cardDescription: response.cardDescription,
+//         });
+//       },
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>
+//           {this.state.cardName}
+//           {this.state.cardImage}
+//           {this.state.cardDescription}
+//         </h1>
+//       </div>
+//     );
+//   }
+// }
