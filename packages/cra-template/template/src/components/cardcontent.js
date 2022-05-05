3702933f8fd4cf39d1d26cb8b206ea280f95c7a3
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-06 01:18:08 +0800

    trying new style

--- packages/cra-template/template/src/components/cardcontent.js
+++ packages/cra-template/template/src/components/cardcontent.js
@@ -1,25 +1,71 @@
+// import React from "react";
+
+// import cardContentData from "../utils/card-content-data";
+// import { Col, Row } from "react-bootstrap";
+
+// const CardContent = () => (
+//   <div className="next-steps my-5">
+//     <h2 className="my-5 text-center">This is CardAI Card page</h2>
+//     <Row className="d-flex justify-content-between">
+//       {cardContentData.map((col, i) => (
+//         <Col key={i} md={5} className="mb-4">
+//           <h6 className="mb-3">
+//             <a target="_blank" rel="noopener noreferrer" href={col.cardImage}>
+//               {col.cardName}
+//             </a>
+//           </h6>
+//           <img src={col.cardImage} class="card__image" />
+//           <p>{col.cardDescription}</p>
+//         </Col>
+//       ))}
+//     </Row>
+//   </div>
+// );
+
+// export default CardContent;
+
 import React from "react";
+import $ from "jquery";
 
-import cardContentData from "../utils/card-content-data";
-import { Col, Row } from "react-bootstrap";
+export default class cardContent extends React.Component {
+  constructor(props) {
+    super(props);
 
-const CardContent = () => (
-  <div className="next-steps my-5">
-    <h2 className="my-5 text-center">This is CardAI Card page</h2>
-    <Row className="d-flex justify-content-between">
-      {cardContentData.map((col, i) => (
-        <Col key={i} md={5} className="mb-4">
-          <h6 className="mb-3">
-            <a target="_blank" rel="noopener noreferrer" href={col.cardImage}>
-              {col.cardName}
-            </a>
-          </h6>
-          <img src={col.cardImage} class="card__image" />
-          <p>{col.cardDescription}</p>
-        </Col>
-      ))}
-    </Row>
-  </div>
-);
+    this.state = {
+      cardName: "",
+      cardImage: "",
+      cardDescription: "",
+    };
+  }
 
-export default CardContent;
+  componentDidMount() {
+    this.fetch();
+  }
+
+  fetch() {
+    var context = this;
+
+    $.ajax({
+      url: "https://calm-lowlands-56636.herokuapp.com/card",
+      method: "GET",
+      success: function (response) {
+        context.setState({
+          cardName: response.cardName,
+          cardImage: response.cardImage,
+          cardDescription: response.cardDescription,
+        });
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.cardName} {this.state.cardImage}{" "}
+          {this.state.cardDescription}
+        </h1>
+      </div>
+    );
+  }
+}
