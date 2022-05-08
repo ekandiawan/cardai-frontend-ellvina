5b287b4ab9773d066b44235dddc56acfd708ff13
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 13:51:08 +0800

    try new method

--- packages/cra-template/template/src/components/personlist.js
+++ packages/cra-template/template/src/components/personlist.js
@@ -28,25 +28,28 @@ export default class PersonList extends React.Component {
       //     )
       //   )}
       // </ul>
-      <div className="next-steps my-5">
-        <Row className="d-flex justify-content-between">
-          {this.state.cards.map((card) => (
-            <Col key={card.id} md={5} className="mb-4">
-              <h6 className="mb-3">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={card.cardImage}
-                >
-                  {card.cardName}
-                </a>
-              </h6>
-              <img src={card.cardImage} class="card__image" />
-              <p>{card.cardDescription}</p>
-            </Col>
-          ))}
-        </Row>
-      </div>
+      <>
+        <div className="next-steps my-5">
+          <Row className="d-flex justify-content-between">
+            {this.state.cards.map(
+              (card, card.id),
+              <Col key={card.id} md={5} className="mb-4">
+                <h6 className="mb-3">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={card.cardImage}
+                  >
+                    {card.cardName}
+                  </a>
+                </h6>
+                <img src={card.cardImage} class="card__image" />
+                <p>{card.cardDescription}</p>
+              </Col>
+            )}
+          </Row>
+        </div>
+      </>
     );
   }
 }
