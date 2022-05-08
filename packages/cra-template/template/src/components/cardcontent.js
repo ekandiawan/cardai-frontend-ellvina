6d4e574ed92a08cd8134417641a2b21030a3bf8b
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 10:04:10 +0800

    use axios in cardcontent

--- packages/cra-template/template/src/components/cardcontent.js
+++ packages/cra-template/template/src/components/cardcontent.js
@@ -1,28 +1,28 @@
-import React from "react";
+// import React from "react";
 
-import cardContentData from "../utils/card-content-data";
-import { Col, Row } from "react-bootstrap";
+// import cardContentData from "../utils/card-content-data";
+// import { Col, Row } from "react-bootstrap";
 
-const CardContent = () => (
-  <div className="next-steps my-5">
-    <h2 className="my-5 text-center">This is CardAI Card page</h2>
-    <Row className="d-flex justify-content-between">
-      {cardContentData.map((col, i) => (
-        <Col key={i} md={5} className="mb-4">
-          <h6 className="mb-3">
-            <a target="_blank" rel="noopener noreferrer" href={col.cardImage}>
-              {col.cardName}
-            </a>
-          </h6>
-          <img src={col.cardImage} class="card__image" />
-          <p>{col.cardDescription}</p>
-        </Col>
-      ))}
-    </Row>
-  </div>
-);
+// const CardContent = () => (
+//   <div className="next-steps my-5">
+//     <h2 className="my-5 text-center">This is CardAI Card page</h2>
+//     <Row className="d-flex justify-content-between">
+//       {cardContentData.map((col, i) => (
+//         <Col key={i} md={5} className="mb-4">
+//           <h6 className="mb-3">
+//             <a target="_blank" rel="noopener noreferrer" href={col.cardImage}>
+//               {col.cardName}
+//             </a>
+//           </h6>
+//           <img src={col.cardImage} class="card__image" />
+//           <p>{col.cardDescription}</p>
+//         </Col>
+//       ))}
+//     </Row>
+//   </div>
+// );
 
-export default CardContent;
+// export default CardContent;
 
 // import React from "react";
 // import $ from "jquery";
@@ -70,3 +70,33 @@ export default CardContent;
 //     );
 //   }
 // }
+
+import React from "react";
+import axios from "axios";
+
+export default class cardContent extends React.Component {
+  state = {
+    cards: [],
+  };
+
+  componentDidMount() {
+    axios.get(`https://calm-lowlands-56636.herokuapp.com/card`).then((res) => {
+      const cards = res.data;
+      this.setState({ cards });
+    });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.cards.map(
+          (card) => (
+            (<li key={card.id}>{card.cardName}</li>),
+            (<li key={card.id}>{card.cardImage}</li>),
+            (<li key={card.id}>{card.cardDescription}</li>)
+          )
+        )}
+      </ul>
+    );
+  }
+}
