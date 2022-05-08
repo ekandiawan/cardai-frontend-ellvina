02ef82beddffed6763370dab75a4b7a6dde7759b
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 11:45:23 +0800

    redo with personlist template

--- packages/cra-template/template/src/components/cardcontent.js
+++ packages/cra-template/template/src/components/cardcontent.js
@@ -1,25 +1,27 @@
 import React from "react";
+import axios from "axios";
 
-import cardContentData from "../utils/card-content-data";
-import { Col, Row } from "react-bootstrap";
+export default class CardContent extends React.Component {
+  state = {
+    cards: [],
+  };
 
-const CardContent = () => (
-  <div className="next-steps my-5">
-    <h2 className="my-5 text-center">This is CardAI Card page</h2>
-    <Row className="d-flex justify-content-between">
-      {cardContentData.map((col, i) => (
-        <Col key={i} md={5} className="mb-4">
-          <h6 className="mb-3">
-            <a target="_blank" rel="noopener noreferrer" href={col.cardImage}>
-              {col.cardName}
-            </a>
-          </h6>
-          <img src={col.cardImage} class="card__image" />
-          <p>{col.cardDescription}</p>
-        </Col>
-      ))}
-    </Row>
-  </div>
-);
+  componentDidMount() {
+    axios.get(`https://calm-lowlands-56636.herokuapp.com/card`).then((res) => {
+      const cards = res.data;
+      this.setState({ cards });
+    });
+  }
 
-export default CardContent;
+  render() {
+    return (
+      <ul>
+        {this.state.cards.map((card) => (
+          <li key={card.id}>
+            {card.cardName} {card.cardImage} {card.cardDescription}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
