b3d8458cce71cde237a30a02fb6624633b46bf1d
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 12:13:34 +0800

    check on personlist js

--- packages/cra-template/template/src/components/personlist.js
+++ packages/cra-template/template/src/components/personlist.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 export default class PersonList extends React.Component {
   state = {
-    cards: [],
+    persons: [],
   };
-  //http://localhost:5000/card
-  //https://jsonplaceholder.typicode.com/users
+
   componentDidMount() {
-    axios.get('https://calm-lowlands-56636.herokuapp.com/card').then(res => {
-      const cards = res.data;
-      this.setState({ cards });
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+      const persons = res.data;
+      this.setState({ persons });
     });
   }
 
   render() {
     return (
       <ul>
-        {this.state.cards.map(card => (
-          <li key={card.id}>{card.cardName}</li>
+        {this.state.persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
         ))}
       </ul>
     );
