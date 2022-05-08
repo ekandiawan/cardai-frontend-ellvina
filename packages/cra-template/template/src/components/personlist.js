e31e44f606d3676b4f6b7853a2679bad24ae2db9
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 15:09:37 +0800

    try again

--- packages/cra-template/template/src/components/personlist.js
+++ packages/cra-template/template/src/components/personlist.js
@@ -16,39 +16,34 @@ export default class PersonList extends React.Component {
 
   render() {
     return (
-      // <ul>
-      //   {this.state.persons.map(
-      //     (person) => (
-      //       (
-      //         <ul key={person.id}>
-      //           {person.name} {person.email}
-      //         </ul>
-      //       ),
-      //       (<ul key={person.id}>{person.email}</ul>)
-      //     )
-      //   )}
-      // </ul>
-      <>
-        <div className="next-steps my-5">
-          <Row className="d-flex justify-content-between">
-            {this.state.cards.map((card) => (
-              <Col key={card.id} md={5} className="mb-4">
-                <h6 className="mb-3">
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={card.cardImage}
-                  >
-                    {card.cardName}
-                  </a>
-                </h6>
-                <img src={card.cardImage} class="card__image" />
-                <p>{card.cardDescription}</p>
-              </Col>
-            ))}
-          </Row>
-        </div>
-      </>
+      <ul>
+        {this.state.cards.map((card) => (
+          <ul key={card.id}>
+            {card.cardName} {card.cardDescription}
+          </ul>
+        ))}
+      </ul>
+      // <>
+      //   <div className="next-steps my-5">
+      //     <Row className="d-flex justify-content-between">
+      //       {this.state.cards.map((card) => (
+      //         <Col key={card.id} md={5} className="mb-4">
+      //           <h6 className="mb-3">
+      //             <a
+      //               target="_blank"
+      //               rel="noopener noreferrer"
+      //               href={card.cardImage}
+      //             >
+      //               {card.cardName}
+      //             </a>
+      //           </h6>
+      //           <img src={card.cardImage} class="card__image" />
+      //           <p>{card.cardDescription}</p>
+      //         </Col>
+      //       ))}
+      //     </Row>
+      //   </div>
+      // </>
     );
   }
 }
