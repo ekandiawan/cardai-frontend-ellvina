7bfcd3d54d1d7066e2aa6dc573829a1cbc07d171
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 12:02:04 +0800

    trial

--- packages/cra-template/template/src/components/cardcontent.js
+++ packages/cra-template/template/src/components/cardcontent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Col, Row } from "react-bootstrap";
 
 export default class CardContent extends React.Component {
   state = {
@@ -15,11 +16,26 @@ export default class CardContent extends React.Component {
 
   render() {
     return (
-      <ul>
-        {this.state.cards.map((card) => (
-          <li key={card.id}>{card.cardName}</li>
-        ))}
-      </ul>
+      <div className="next-steps my-5">
+        <h2 className="my-5 text-center">This is CardAI Card page</h2>
+        <Row className="d-flex justify-content-between">
+          {cards.map((col, id) => (
+            <Col key={id} md={5} className="mb-4">
+              <h6 className="mb-3">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={col.cardImage}
+                >
+                  {col.cardName}
+                </a>
+              </h6>
+              <img src={col.cardImage} class="card__image" />
+              <p>{col.cardDescription}</p>
+            </Col>
+          ))}
+        </Row>
+      </div>
     );
   }
 }
