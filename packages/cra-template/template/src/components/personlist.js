8479ad35ab5b3120b2e03500fe713de7fd235501
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 15:17:10 +0800

    try try

--- packages/cra-template/template/src/components/personlist.js
+++ packages/cra-template/template/src/components/personlist.js
@@ -18,32 +18,32 @@ export default class PersonList extends React.Component {
     return (
       <ul>
         {this.state.cards.map((card) => (
-          <ul key={card.id}>
-            {card.cardName} {card.cardDescription}
-          </ul>
+          <li key={card.id}>{card.cardName}</li>
         ))}
       </ul>
-      // <>
-      //   <div className="next-steps my-5">
-      //     <Row className="d-flex justify-content-between">
-      //       {this.state.cards.map((card) => (
-      //         <Col key={card.id} md={5} className="mb-4">
-      //           <h6 className="mb-3">
-      //             <a
-      //               target="_blank"
-      //               rel="noopener noreferrer"
-      //               href={card.cardImage}
-      //             >
-      //               {card.cardName}
-      //             </a>
-      //           </h6>
-      //           <img src={card.cardImage} class="card__image" />
-      //           <p>{card.cardDescription}</p>
-      //         </Col>
-      //       ))}
-      //     </Row>
-      //   </div>
-      // </>
     );
   }
+  // <>
+  //   <div className="next-steps my-5">
+  //     <Row className="d-flex justify-content-between">
+  //       {this.state.cards.map((card) => (
+  //         <Col key={card.id} md={5} className="mb-4">
+  //           <h6 className="mb-3">
+  //             <a
+  //               target="_blank"
+  //               rel="noopener noreferrer"
+  //               href={card.cardImage}
+  //             >
+  //               {card.cardName}
+  //             </a>
+  //           </h6>
+  //           <img src={card.cardImage} class="card__image" />
+  //           <p>{card.cardDescription}</p>
+  //         </Col>
+  //       ))}
+  //     </Row>
+  //   </div>
+  // </>
+  //   );
+  // }
 }
