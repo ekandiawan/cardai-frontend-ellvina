17bf52c7c85544fcdc2783742f3339586e3291bc
Ellvina Kandiawan <ellvina.kandiawan@gmail.com>
2022-05-08 12:30:32 +0800

    change personlist to card

--- packages/cra-template/template/src/components/personlist.js
+++ packages/cra-template/template/src/components/personlist.js
@@ -1,32 +1,52 @@
 import React from "react";
 import axios from "axios";
+import { Col, Row } from "react-bootstrap";
 
-export default class PersonList extends React.Component {
+export default class CardContent extends React.Component {
   state = {
-    persons: [],
+    cards: [],
   };
 
   componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
-      const persons = res.data;
-      this.setState({ persons });
+    axios.get(`https://calm-lowlands-56636.herokuapp.com/card`).then((res) => {
+      const cards = res.data;
+      this.setState({ cards });
     });
   }
 
   render() {
     return (
-      <ul>
-        {this.state.persons.map(
-          (person) => (
-            (
-              <ul key={person.id}>
-                {person.name} {person.email}
-              </ul>
-            ),
-            (<ul key={person.id}>{person.email}</ul>)
-          )
-        )}
-      </ul>
+      // <ul>
+      //   {this.state.persons.map(
+      //     (person) => (
+      //       (
+      //         <ul key={person.id}>
+      //           {person.name} {person.email}
+      //         </ul>
+      //       ),
+      //       (<ul key={person.id}>{person.email}</ul>)
+      //     )
+      //   )}
+      // </ul>
+      <div className="next-steps my-5">
+        <Row className="d-flex justify-content-between">
+          {this.state.cards.map((card) => (
+            <Col key={card.id} md={5} className="mb-4">
+              <h6 className="mb-3">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={card.cardImage}
+                >
+                  {card.cardName}
+                </a>
+              </h6>
+              <img src={card.cardImage} class="card__image" />
+              <p>{card.cardDescription}</p>
+            </Col>
+          ))}
+        </Row>
+      </div>
     );
   }
 }
